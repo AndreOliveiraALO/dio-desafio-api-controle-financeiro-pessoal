f9f23fa01a98f4ae8c413922bd3a601d8c0d1cb7
André Luiz de Oliveira <andreloliveira28@gmail.com>
2025-08-26 20:07:35 -0300

    Teste e correções, indetação e remoção dos comentarios nas service e controller da entidade Despesa.

--- fincontrol/src/main/java/com/santander/dio/fincontrol/controller/DespesaController.java
+++ fincontrol/src/main/java/com/santander/dio/fincontrol/controller/DespesaController.java
@@ -2,6 +2,7 @@ package com.santander.dio.fincontrol.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,7 +18,7 @@ import com.santander.dio.fincontrol.dto.response.DespesaResponse;
 import com.santander.dio.fincontrol.service.DespesaService;
 
 @RestController
-@RequestMapping("/api/despesas")
+@RequestMapping("/despesas")
 public class DespesaController {
 
     private final DespesaService despesaService;
@@ -27,18 +28,18 @@ public class DespesaController {
     }
 
     @PostMapping
-    public ResponseEntity<DespesaResponse> salvar(@RequestBody DespesaRequest dto) {
-        return ResponseEntity.ok(despesaService.salvar(dto));        
+    public ResponseEntity<DespesaResponse> salvar(
+            @RequestBody DespesaRequest dto) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(despesaService.salvar(dto));        
     }
 
     @GetMapping
     public ResponseEntity<List<DespesaResponse>> listar() {
-        return ResponseEntity.ok(despesaService.listar());
-
-        /*List<DespesaResponse> despesas = despesaService.listar().stream()
-                .map(DespesaResponse::fromEntity)
-                .toList();
-        return ResponseEntity.ok(despesas);*/
+        List<DespesaResponse> lista = despesaService.listar();
+        return lista.isEmpty() 
+            ?ResponseEntity.noContent().build() 
+            :ResponseEntity.ok(lista);
     }
 
     @GetMapping("/{id}")
@@ -47,7 +48,8 @@ public class DespesaController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<DespesaResponse> atualizar(@PathVariable Long id, @RequestBody DespesaRequest request) {
+    public ResponseEntity<DespesaResponse> atualizar(
+            @PathVariable Long id, @RequestBody DespesaRequest request) {
         return ResponseEntity.ok(despesaService.atualizar(id, request));
     }
 
--- fincontrol/src/main/java/com/santander/dio/fincontrol/service/DespesaService.java
+++ fincontrol/src/main/java/com/santander/dio/fincontrol/service/DespesaService.java
@@ -1,16 +1,20 @@
 package com.santander.dio.fincontrol.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
 import com.santander.dio.fincontrol.dto.request.DespesaRequest;
+import com.santander.dio.fincontrol.dto.response.CategoriaResponse;
 import com.santander.dio.fincontrol.dto.response.DespesaResponse;
+import com.santander.dio.fincontrol.exception.RecursoNaoEncontradoException;
 import com.santander.dio.fincontrol.model.Categoria;
 import com.santander.dio.fincontrol.model.Despesa;
 import com.santander.dio.fincontrol.model.Usuario;
 import com.santander.dio.fincontrol.repository.CategoriaRepository;
 import com.santander.dio.fincontrol.repository.TransacaoRepository;
 import com.santander.dio.fincontrol.repository.UsuarioRepository;
-import org.springframework.stereotype.Service;
-import java.util.List;
-import com.santander.dio.fincontrol.exception.RecursoNaoEncontradoException;
+import com.santander.dio.fincontrol.utils.TipoCategoria;
 
 @Service
 public class DespesaService {
@@ -25,25 +29,11 @@ public class DespesaService {
         this.categoriaRepository = categoriaRepository;
     }
 
-    public DespesaResponse salvar(DespesaRequest dto) {
-        Usuario usuario = usuarioRepository.findById(dto.usuarioId())
-                .orElseThrow(() -> new RecursoNaoEncontradoException("Usuário não encontrado"));
-
-        Categoria categoria = categoriaRepository.findById(dto.categoriaId())
-                .orElseThrow(() -> new RecursoNaoEncontradoException("Categoria não encontrada"));
-
-        Despesa despesa = Despesa.fromRequest(dto, usuario, categoria);
-        transacaoRepository.save(despesa);
-
-        return DespesaResponse.fromEntity(despesa);
-    }
-
     public List<DespesaResponse> listar() {
         return transacaoRepository.findAll().stream()
                 .filter(t -> t instanceof Despesa)
                 .map(t -> (Despesa) t)
-                .map(DespesaResponse::fromEntity)
-                //.map(this::toResponse)
+                .map(DespesaResponse::fromEntity)                
                 .toList();
     }
 
@@ -51,19 +41,50 @@ public class DespesaService {
         Despesa despesa = transacaoRepository.findById(id)
                 .filter(t -> t instanceof Despesa)
                 .map(t -> (Despesa) t)
-                .orElseThrow(() -> new RecursoNaoEncontradoException("Despesa não encontrada com id " + id));
+                .orElseThrow(() -> new RecursoNaoEncontradoException(
+                    "Despesa não encontrada com id " + id));
+        return DespesaResponse.fromEntity(despesa);
+    }
+
+    public List<CategoriaResponse> buscarPor(TipoCategoria tipo){
+        List<Categoria> categorias = categoriaRepository.findByTipo(tipo);
+          
+        if (categorias.isEmpty())
+            throw new RecursoNaoEncontradoException(
+                "Nenhuma categoria encontrada do tipo "+tipo);
+        
+        return categorias.stream()
+            .map(CategoriaResponse::fromEntity)
+            .toList();            
+    }
+
+    public DespesaResponse salvar(DespesaRequest dto) {
+        Usuario usuario = usuarioRepository.findById(dto.usuarioId())
+                .orElseThrow(() -> new RecursoNaoEncontradoException(
+                    "Usuário não encontrado"));
+
+        Categoria categoria = categoriaRepository.findById(dto.categoriaId())
+                .orElseThrow(() -> new RecursoNaoEncontradoException(
+                    "Categoria não encontrada"));
+
+        Despesa despesa = Despesa.fromRequest(dto, usuario, categoria);
+        transacaoRepository.save(despesa);
+
         return DespesaResponse.fromEntity(despesa);
     }
 
     public DespesaResponse atualizar(Long id, DespesaRequest dto) {
         if (!transacaoRepository.existsById(id))
-            throw new RecursoNaoEncontradoException("Despesa não encontrado com id " + id);
+            throw new RecursoNaoEncontradoException(
+                "Despesa não encontrado com id " + id);
 
         Usuario usuario = usuarioRepository.findById(dto.usuarioId())
-                .orElseThrow(() -> new RecursoNaoEncontradoException("Usuário não encontrado"));
+                .orElseThrow(() -> new RecursoNaoEncontradoException(
+                    "Usuário não encontrado"));
 
         Categoria categoria = categoriaRepository.findById(dto.categoriaId())
-                .orElseThrow(() -> new RecursoNaoEncontradoException("Categoria não encontrada"));
+                .orElseThrow(() -> new RecursoNaoEncontradoException(
+                    "Categoria não encontrada"));
         
         Despesa despesa = Despesa.fromRequest(dto, usuario, categoria);
         despesa.setId(id);        
@@ -72,7 +93,8 @@ public class DespesaService {
 
     public void deletar(Long id) {
         if (!transacaoRepository.existsById(id))
-            throw new RecursoNaoEncontradoException("Despesa não encontrado com id " + id);
+            throw new RecursoNaoEncontradoException(
+                "Despesa não encontrado com id " + id);
         transacaoRepository.deleteById(id);
     }
 }
